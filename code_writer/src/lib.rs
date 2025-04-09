614f648e9249809f7dc9eb922ce4e7ee0e23d274
t9a <t9a.dev@icloud.com>
2025-04-09 21:56:31 +0900

    wip

--- code_writer/src/lib.rs
+++ code_writer/src/lib.rs
@@ -28,32 +28,25 @@ impl CodeWriter {
     }
 
     pub fn write_arithmetic(&mut self, command: &str) -> Result<()> {
+        let is_single_operand = command == "neg" || command == "not";
         let variable_register = VariableRegister::R13;
         self.write_pop()?;
         self.write_load_register(variable_register)?;
-        self.write_pop()?;
+        // オペランドが1つの場合はyのみスタックからpopする
+        if !is_single_operand {
+            self.write_pop()?;
+        }
 
         // Arithmetic
-        let operator: String = unindent(
-            format!(
-                r#"{}
-        D=D{}M
-        @TRUE
-        {}
-        D=0
-        @PUSH
-        0;JMP
-        (TRUE)
-        D=-1
-        (PUSH)
-        "#,
-                variable_register.as_ref(),
-                "", //TODO
-                "", //TODO
-            )
-            .as_str(),
-        );
-        self.assembly_file.write(operator.as_bytes())?;
+        let arithmetic_command = self
+            .get_arithmetic_command(command, &variable_register)
+            .or_else(|_| {
+                self.get_comparison_command(command, &variable_register)
+                    .or_else(|_| self.get_logical_command(command, &variable_register))
+            })
+            .expect(format!("no support command. command: {}", command).as_str())
+            .unwrap();
+        self.assembly_file.write(arithmetic_command.as_bytes())?;
 
         self.write_push()?;
 
@@ -84,9 +77,101 @@ impl CodeWriter {
         drop(self.assembly_file)
     }
 
+    fn get_arithmetic_command(
+        &self,
+        command: &str,
+        variable_register: &VariableRegister,
+    ) -> Result<Option<String>> {
+        let operator: Option<&str> = match command {
+            "add" => Some("\n// add\nD=D+M\n"),
+            "sub" => Some("\n// sub\nD=D-M\n"),
+            "neg" => Some("\n// neg\nD=-M\n"),
+            &_ => None,
+        };
+
+        if operator.is_none() {
+            return Ok(None);
+        }
+
+        Ok(Some(unindent(
+            format!(
+                "@{}\n{}",
+                variable_register.as_ref(),
+                operator.unwrap(),
+            )
+            .as_str(),
+        )))
+    }
+
+    fn get_comparison_command(
+        &self,
+        command: &str,
+        variable_register: &VariableRegister,
+    ) -> Result<Option<String>> {
+        let comp_operator: Option<&str> = match command {
+            "eq" => Some("\n// eq\nD;JEQ\n"),
+            "gt" => Some("\n// gt\nD;JGT\n"),
+            "lt" => Some("\n// lt\nD;JLT\n"),
+            &_ => None,
+        };
+
+        if comp_operator.is_none() {
+            return Ok(None);
+        }
+
+        Ok(Some(unindent(
+            format!(
+                r#"
+        {}
+        D=D-M
+        @TRUE
+        {}
+        D=0
+        @PUSH
+        0;JMP
+        (TRUE)
+        D=-1
+        (PUSH)
+        "#,
+                variable_register.as_ref(),
+                comp_operator.unwrap(),
+            )
+            .as_str(),
+        )))
+    }
+
+    fn get_logical_command(
+        &self,
+        command: &str,
+        variable_register: &VariableRegister,
+    ) -> Result<Option<String>> {
+        let operator: Option<&str> = match command {
+            "and" => Some("\n// and\nD=D&M\n"),
+            "or" => Some("\n// or\nD=D|M\n"),
+            "not" => Some("\n// not\nD=!M\n"),
+            &_ => None,
+        };
+
+        if operator.is_none() {
+            return Ok(None);
+        }
+
+        Ok(Some(unindent(
+            format!(
+                r#"
+        {}
+        {}
+        "#,
+                variable_register.as_ref(),
+                operator.unwrap(),
+            )
+            .as_str(),
+        )))
+    }
+
     fn write_segment(&mut self, command: CommandType, segment: &str, index: u16) -> Result<()> {
         let segment_symbol_asm = match segment {
-            "local" => Some(format!("// that {}\n@LCL\n", index)),
+            "local" => Some(format!("// local {}\n@LCL\n", index)),
             "argument" => Some(format!("// argument {}\n@ARG\n", index)),
             "this" => Some(format!("// this {}\n@THIS\n", index)),
             "that" => Some(format!("// that {}\n@THAT\n", index)),
@@ -102,7 +187,7 @@ impl CodeWriter {
         };
 
         let segment_access_asm = format!(
-            "{}A=M+{}\n",
+            "\n{}A=M+{}\n",
             segment_symbol_asm.expect("not support segment"),
             index
         );
@@ -122,7 +207,9 @@ impl CodeWriter {
     }
 
     fn write_push(&mut self) -> Result<()> {
-        let push_asm = r#"// push
+        let push_asm = r#"
+
+       // push
        @SP
        A=M
        M=D
@@ -134,11 +221,13 @@ impl CodeWriter {
     }
 
     fn write_pop(&mut self) -> Result<()> {
-        let pop_asm = r#"// pop
-       @SP
-       M=M-1
-       A=M
-       D=M
+        let pop_asm = r#"
+
+        // pop
+        @SP
+        M=M-1
+        A=M
+        D=M
        "#;
         self.assembly_file.write(unindent(&pop_asm).as_bytes())?;
         Ok(())
@@ -146,7 +235,7 @@ impl CodeWriter {
 
     fn write_load_register(&mut self, register: VariableRegister) -> Result<()> {
         self.assembly_file
-            .write(format!("{}\nM=D", register.as_ref()).as_bytes())?;
+            .write(format!("@{}\nM=D\n", register.as_ref()).as_bytes())?;
 
         Ok(())
     }
@@ -154,9 +243,22 @@ impl CodeWriter {
 
 #[cfg(test)]
 mod tests {
-    use std::io::Read;
+    use pretty_assertions::assert_eq;
+    use rand::distr::{Alphanumeric, SampleString};
+    use std::{fs, io::Read};
 
     use super::*;
+
+    fn get_code_writer() -> Result<(CodeWriter, String)> {
+        fs::create_dir_all("../target/test/data")?;
+        let mut test_file_name = Alphanumeric.sample_string(&mut rand::rng(), 5);
+        test_file_name = format!("{}.vm", test_file_name);
+        let file_path = Path::new("../target/test/data").join(&test_file_name);
+        Ok((
+            CodeWriter::new(&file_path),
+            file_path.to_string_lossy().to_string(),
+        ))
+    }
 
     #[test]
     fn playground() -> Result<()> {
@@ -165,20 +267,37 @@ mod tests {
     }
 
     #[test]
-    fn test_push_command() -> Result<()> {
-        let test_file_name = "Test.vm";
-        {
-            let mut code_writer = CodeWriter::new(&Path::new(test_file_name));
-            code_writer.write_push_pop(CommandType::Push, "that", 5)?;
-        }
+    fn test_write_segment() -> Result<()> {
+        let (mut code_writer, test_file_name) = get_code_writer()?;
+        code_writer.write_segment(CommandType::Push, "that", 5)?;
 
         let mut asm_file_content = String::new();
-        File::open(test_file_name)?.read_to_string(&mut asm_file_content)?;
+        File::open(&test_file_name)?.read_to_string(&mut asm_file_content)?;
 
         let expect_asm = r#"// that 5
         @THAT
         A=M+5
         D=M
+        "#;
+        assert_eq!(unindent(expect_asm), unindent(&asm_file_content));
+
+        fs::remove_file(test_file_name)?;
+        Ok(())
+    }
+
+    #[test]
+    fn test_push_command() -> Result<()> {
+        let (mut code_writer, test_file_name) = get_code_writer()?;
+        code_writer.write_push_pop(CommandType::Push, "that", 5)?;
+
+        let mut asm_file_content = String::new();
+        File::open(&test_file_name)?.read_to_string(&mut asm_file_content)?;
+
+        let expect_asm = r#"// that 5
+        @THAT
+        A=M+5
+        D=M
+
         // push
         @SP
         A=M
@@ -188,6 +307,71 @@ mod tests {
         "#;
         assert_eq!(unindent(expect_asm), unindent(&asm_file_content));
 
+        fs::remove_file(test_file_name)?;
+        Ok(())
+    }
+
+    #[test]
+    fn test_pop_command() -> Result<()> {
+        let (mut code_writer, test_file_name) = get_code_writer()?;
+        code_writer.write_push_pop(CommandType::Pop, "local", 0)?;
+
+        let mut asm_file_content = String::new();
+        File::open(&test_file_name)?.read_to_string(&mut asm_file_content)?;
+
+        let expect_asm = r#"// pop
+        @SP
+        M=M-1
+        A=M
+        D=M
+
+        // local 0
+        @LCL
+        A=M+0
+        M=D
+        "#;
+        assert_eq!(unindent(expect_asm), unindent(&asm_file_content));
+
+        fs::remove_file(test_file_name)?;
+        Ok(())
+    }
+
+    #[test]
+    fn test_write_arithmetic() -> Result<()> {
+        let (mut code_writer, test_file_name) = get_code_writer()?;
+        code_writer.write_arithmetic("add")?;
+
+        let mut asm_file_content = String::new();
+        File::open(&test_file_name)?.read_to_string(&mut asm_file_content)?;
+
+        let expect_asm = r#"// pop
+        @SP
+        M=M-1
+        A=M
+        D=M
+        @R13
+        M=D
+
+        // pop
+        @SP
+        M=M-1
+        A=M
+        D=M
+        @R13
+
+        // add
+        D=D+M
+
+        // push
+        @SP
+        A=M
+        M=D
+        @SP
+        M=M+1
+        "#;
+        assert_eq!(unindent(expect_asm), unindent(&asm_file_content));
+
+        fs::remove_file(test_file_name)?;
         Ok(())
     }
 }
